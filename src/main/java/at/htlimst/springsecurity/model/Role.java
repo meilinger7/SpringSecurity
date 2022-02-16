de29f351c4e680c0841167441c3ca5241a67b6f2
meilinger7 <julianmeilinger07@gmail.com>
2022-02-16 15:40:43 +0100

    db conn

---
+++ src/main/java/at/htlimst/springsecurity/model/Role.java
@@ -0,0 +1,25 @@
+package at.htlimst.springsecurity.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
